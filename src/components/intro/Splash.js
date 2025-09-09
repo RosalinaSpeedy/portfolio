4e6b7c9604a93031c77a143401b140c59b4f17d1
RosalinaSpeedy <86687184+RosalinaSpeedy@users.noreply.github.com>
2025-09-09 19:55:07 +0100

    SPLASH SCREEN layout and basic css done

--- src/components/intro/Splash.js
+++ src/components/intro/Splash.js
@@ -6,14 +6,24 @@ const Splash = () => {
     <div class="splashContainer">
       <div class="splashImage">
         <div class="splashText">
-            BEN CRADDOCK
+          BEN CRADDOCK
         </div>
         <div class="splashTagline">
-            FULL STACK, APP AND AI DEVELOPER
+          FULL STACK, APP AND AI DEVELOPER
         </div>
-        <div class="arrowDownContainer">
-
-        </div>
+      </div>
+      <div class="splashExtraTextContainer">
+        <div class="commentContainer"><div class="commentNumber">25</div><div class="extraComment">/*</div></div>
+        <div class="commentContainer"><div class="commentNumber verticalCommentLine">26</div><div class="longComment">Can someone please write unit tests for this portfolio page?</div></div>
+        <div class="commentContainer"><div class="commentNumber verticalCommentLine">27</div><div class="longComment">You're having a laugh mate.</div></div>
+        <div class="commentContainer highlightedComment"><div class="highlightedNumber">28</div><div class="extraComment">*/</div></div>
+      </div>
+      <div class="arrowDownContainer">
+        <svg class="arrows">
+          <path class="a1" d="M0 0 L22.5 32 L50 0"></path>
+          <path class="a2" d="M0 20 L22.5 52 L50 20"></path>
+          <path class="a3" d="M0 40 L22.5 72 L50 40"></path>
+        </svg>
       </div>
     </div>
   )
