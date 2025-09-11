0b3c02052daae85b9ea04c47156dad9fff886d18
BenC <86687184+RosalinaSpeedy@users.noreply.github.com>
2025-09-11 01:29:13 +0100

    responsive animation

--- src/components/intro/Splash.js
+++ src/components/intro/Splash.js
@@ -93,7 +93,7 @@ const Splash = () => {
         <div class={comment4Container}><div class={comment4Number}>28</div><div class="extraComment">{comment4Typed}</div></div>
       </div>
       <div class="arrowDownContainer">
-        <svg class="arrows">
+        <svg viewBox="0 0 47 80" class="arrows">
           <path class="a1" d="M0 0 L23 32 L46 0"></path>
           <path class="a2" d="M0 20 L23 52 L46 20"></path>
           <path class="a3" d="M0 40 L23 72 L46 40"></path>
