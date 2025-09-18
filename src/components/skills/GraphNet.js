1a3af769606661b664f5011708d179d48dad1f1d
RosalinaSpeedy <86687184+RosalinaSpeedy@users.noreply.github.com>
2025-09-18 20:18:16 +0100

    hover fx for graph

--- src/components/skills/GraphNet.js
+++ src/components/skills/GraphNet.js
@@ -6,37 +6,90 @@ import GraphNode from './GraphNode';
 
 const GraphNet = () => {
     return (
-        <FadeInElement><div class="graphContainer">
-            <GraphNode
-                r={30}
-                cx={80}
-                cy={43}
-            />
-            <GraphNode
-                r={30}
-                cx={100}
-                cy={290}
-            />
-            <GraphNode
-                r={30}
-                cx={200}
-                cy={180}
-            />
-            <GraphNode
-                r={30}
-                cx={40}
-                cy={180}
-            />
-            <GraphNode
-                r={30}
-                cx={450}
-                cy={300}
-            />
-            <GraphNode
-                r={30}
-                cx={400}
-                cy={90}
-            />
+        <FadeInElement><div id="graphContainer">
+            <svg class="circleContainer" xmlns="http://www.w3.org/2000/svg">
+                <GraphNode
+                    r={30}
+                    cx={110}
+                    cy={81}
+                    lines={[
+                        {
+                            x1: 105,
+                            y1: 83,
+                            x2: 200,
+                            y2: 180
+                        },
+                    ]}
+                />
+                <GraphNode
+                    r={30}
+                    cx={175}
+                    cy={290}
+                    lines={[
+                        {
+                            x1: 175,
+                            y1: 295,
+                            x2: 200,
+                            y2: 180
+                        },
+                    ]}
+                />
+                
+                <GraphNode
+                    r={30}
+                    cx={90}
+                    cy={200}
+                    lines={[
+                        {
+                            x1: 90,
+                            y1: 200,
+                            x2: 170,
+                            y2: 180
+                        },
+                    ]}
+                />
+                <GraphNode
+                    r={30}
+                    cx={400}
+                    cy={90}
+                    lines={[
+                        {
+                            x1: 400,
+                            y1: 90,
+                            x2: 220,
+                            y2: 158
+                        },
+                        {
+                            x1: 400,
+                            y1: 90,
+                            x2: 400,
+                            y2: 270
+                        },
+                    ]}
+                />
+                <GraphNode
+                    r={30}
+                    cx={400}
+                    cy={285}
+                    lines={[
+                        {
+                            x1: 400,
+                            y1: 300,
+                            x2: 220,
+                            y2: 200
+                        },
+                    ]}
+                />
+                
+                <GraphNode
+                    r={30}
+                    cx={200}
+                    cy={180}
+                    lines={[
+
+                    ]}
+                />
+            </svg>
         </div></FadeInElement>
     )
 }
--- src/components/skills/GraphNode.js
+++ src/components/skills/GraphNode.js
@@ -3,11 +3,25 @@ import skills from './skills.css';
 
 import FadeInElement from '../common/FadeInElement';
 
-const GraphNode = ({ id, r, cx, cy }) => {
+const GraphNode = ({ id, r, cx, cy, lines }) => {
+
+    const renderLines = () => {
+        const linesMapped = lines.map(line => <line
+            class="graphLine" x1={line.x1} y1={line.y1} x2={line.x2} y2={line.y2} key={"" + line.x1 + line.y1 + line.x2 + line.y2}
+        />)
+        return linesMapped
+    }
+    const buildGraphic = () => {
+        return (
+            <svg>
+                {renderLines()}
+                <circle r={r} cx={cx} cy={cy} class="circle" />
+            </svg>
+        )
+    }
     return (
-        <svg class="circleContainer">
-            <circle r={r} cx={cx} cy={cy} fill="#569CD6" stroke="#CCCCCC" stroke-width="3" />
-        </svg>
+        //<circle r={r} cx={cx} cy={cy} class="circle" />
+        buildGraphic()
     )
 }
 
